5e7c833611d9f6b7d8f3e6ce77cc987342204559
Gabriel Barn <gbrielbbarn@gmail.com>
2024-05-21 18:06:39 -0300

    [feat] theme toggle using local storage

---
+++ javascript/script.js
@@ -0,0 +1,33 @@
+const root = document.documentElement;
+const headerLogo = document.querySelector('#header-logo');
+const themeCheckbox = document.querySelector('#theme-checkbox');
+const themeCheckboxLabel = document.querySelector('#theme-checkbox-label');
+
+if (localStorage.getItem('theme') === 'light') {
+    root.classList.remove('dark');
+    root.classList.add('light');
+    headerLogo.src = 'assets/odin-lined-black.png';
+    themeCheckbox.checked = true;
+} else {
+    root.classList.remove('light');
+    root.classList.add('dark');
+    headerLogo.src = 'assets/odin-lined-white.png';
+    themeCheckbox.checked = false;
+};
+
+const changeTheme = () => {
+    if (themeCheckbox.checked) {
+        root.classList.remove('dark');
+        root.classList.add('light');
+        localStorage.setItem('theme', 'light');
+        headerLogo.src = 'assets/odin-lined-black.png';
+    } else {
+        root.classList.remove('light');
+        root.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+        headerLogo.src = 'assets/odin-lined-white.png';
+    }
+}
+
+themeCheckbox.addEventListener('click', changeTheme);
+
